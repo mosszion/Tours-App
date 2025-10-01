829a4a8f81d6134467b7e5a56d42e1e0cc7a59bc
mosszion <musdzion@gmail.com>
2025-10-01 05:59:56 -0400

    maps tours and passed props to tour component

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Tour from './Tour';
 
-const Tours = () => {
+const Tours = ({ tours, removeTour }) => {
   return (
-    <div>
-      <h2>Our Tours</h2>
+    <section>   
+        <div className='title'> 
+        <h2>our tours</h2>
+        <div className='title-underline'></div>
+        </div>
+    <div className='tours'>
+        {tours.map((tour) => {
+           
+            return <Tour key={tour.id} tour={tour} removeTour={removeTour} />
+
+        })}
+    
     </div>
+    </section>
   )
 }
 
