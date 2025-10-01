69936107dadb2bf16cb10a31fa099d926acfb0b9
mosszion <musdzion@gmail.com>
2025-10-01 06:01:56 -0400

    setup the tour component and added readMore state

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Tour = () => {
-  return (
-    <div>
-      <h1>Tour Component</h1>
-    </div>
+const Tour = ({tour, removeTour}) => {
+ const { id, image, info, price, name } = tour;
+
+ const [readMore, setReadMore]=useState(false);
+
+ 
+    return (
+< article className='single-tour'>
+        <img src={image} alt={name} className='img'/>
+         <span className='tour-price'>${price}</span>
+        <div className='tour-info'>
+            <h5>{name}</h5>
+            <p>{readMore ? info : `${info.substring(0, 200)}...`}
+            <button className='info-btn' onClick={() => setReadMore(!readMore)}>
+                {readMore ? 'Show Less' : 'Read More'}
+            </button>
+            </p>
+          <button className='delete-btn btn btn-block' onClick={() => {removeTour(id)}}>Not Interested</button>      
+       
+
+        </div>
+    </article>
   )
 }
 
